7648b3ee3decd40db3b542f45922c3fbaef66b75
AnotheerDev <leger.julien94@gmail.com>
2024-02-28 19:20:28 +0100

    Add weather functionality to bot

--- index.js
+++ index.js
@@ -26,6 +26,21 @@ client.on('messageCreate', message => {
   if (message.content === '!ping') {
       console.log('Commande !ping détectée');
       message.channel.send('Pong.');
+  } else if (message.content.startsWith('!meteo')) {
+    // Obtient le nom de la ville après la commande !meteo
+      const ville = message.content.split(" ")[1];
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${ville}&appid=${process.env.OPENWEATHERMAP_API_KEY}&units=metric&lang=fr`;
+
+      axios.get(url)
+        .then(response => {
+            const temp = response.data.main.temp;
+            const weatherDescription = response.data.weather[0].description;
+            message.reply(`La météo à ${ville} est de ${temp}°C avec ${weatherDescription}.`);
+        })
+        .catch(error => {
+            console.error(error);
+            message.reply("Désolé, je n'ai pas pu trouver la météo pour cette ville.");
+        });
   }
 });
 
